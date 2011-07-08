24a5d4c34a27ebac387c0f82bb31c563f2735a4b
Michael Mosmann <michael@mosmann.de>
2011-07-08 07:34:26 +0200

    mapping refactor

---
+++ src/main/java/de/flapdoodle/mongoom/parser/Datastore.java
@@ -0,0 +1,88 @@
+/**
+ * Copyright (C) 2010 Michael Mosmann <michael@mosmann.de>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.flapdoodle.mongoom.parser;
+
+import java.util.List;
+
+import com.mongodb.DB;
+import com.mongodb.Mongo;
+
+import de.flapdoodle.mongoom.IDatastore;
+import de.flapdoodle.mongoom.IEntityQuery;
+import de.flapdoodle.mongoom.mapping.IMappingConfig;
+
+public class Datastore implements IDatastore {
+
+	private Mapper _mapper;
+	private Mongo _mongo;
+	private String _name;
+	private DB _db;
+
+	public Datastore(Mapper mapper, Mongo mongo, String name) {
+		_mapper = mapper;
+		_mongo = mongo;
+		_name = name;
+
+		_db = _mongo.getDB(_name);
+	}
+
+	@Override
+	public void ensureCaps() {
+		notImplemented();
+	}
+
+	@Override
+	public void ensureIndexes() {
+		notImplemented();
+	}
+
+	@Override
+	public <T> void insert(T entity) {
+		notImplemented();
+	}
+
+	@Override
+	public <T> void save(T entity) {
+		notImplemented();
+	}
+
+	@Override
+	public <T> void update(T entity) {
+		notImplemented();
+	}
+
+	@Override
+	public <T> void delete(T entity) {
+		notImplemented();
+	}
+
+	@Override
+	public <T> List<T> find(Class<T> entityClass) {
+		notImplemented();
+		return null;
+	}
+
+	@Override
+	public <T> IEntityQuery<T> with(Class<T> entityClass) {
+		notImplemented();
+		return null;
+	}
+
+	private static void notImplemented() {
+		throw new IllegalArgumentException("not implemented");
+	}
+}
---
+++ src/main/java/de/flapdoodle/mongoom/parser/naming/PropertyNamingListFactory.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (C) 2010 Michael Mosmann <michael@mosmann.de>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.flapdoodle.mongoom.parser.naming;
+
+import java.util.Collection;
+import java.util.List;
+
+import com.google.common.collect.Lists;
+
+import de.flapdoodle.mongoom.parser.IFieldType;
+
+
+public class PropertyNamingListFactory implements IPropertyNamingFactory {
+
+	List<? extends IPropertyNamingFactory> _factories;
+	
+	public PropertyNamingListFactory(Collection<? extends IPropertyNamingFactory> factories) {
+		_factories=Lists.newArrayList(factories);
+	}
+	
+	@Override
+	public String getPropertyName(IFieldType fieldType) {
+		String ret=null;
+		for (IPropertyNamingFactory factory : _factories) {
+			ret=factory.getPropertyName(fieldType);
+			if (ret!=null) break;
+		}
+		return ret;
+	}
+}
