1dcf8d6e20cfb317eeeae9c47c9ce655b81ace83
Michael Mosmann <michael@mosmann.de>
2011-09-01 18:39:02 +0200

    testlab

--- src/main/java/de/flapdoodle/mongoom/testlab/Property.java
+++ src/main/java/de/flapdoodle/mongoom/testlab/Property.java
@@ -2,15 +2,29 @@ package de.flapdoodle.mongoom.testlab;
 
 import java.lang.reflect.Type;
 
-
 public class Property<T> {
+
 	private final String _name;
 	private final Class<T> _type;
 	private final Type _genericType;
+	
+	// MetaInfos (Index?)
 
 	public Property(String name, Class<T> type, Type genericType) {
 		_name = name;
 		_type = type;
 		_genericType = genericType;
+	}
+
+	public String getName() {
+		return _name;
+	}
+
+	public Class<T> getType() {
+		return _type;
+	}
+
+	public Type getGenericType() {
+		return _genericType;
 	}
 }
